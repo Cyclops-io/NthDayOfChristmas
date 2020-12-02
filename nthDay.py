90dd1c1241ca34689be26288164f2586033536b9
Christian <mrchristianw@gmail.com>
2020-12-01 21:10:16 -0500

    Create nthDay.py

---
+++ nthDay.py
@@ -0,0 +1,6 @@
+import inflect
+giftArr = ["Partridge in a pear-tree", "Turtle doves", "French hens","Colly birds","Gold rings","Geese a laying", "Swans a swimming","Maids a milking","Drummers drumming","Pipers piping","Ladies dancing","Lords a leaping"]
+def printNthDay(int n):
+    print("On the " +
+
+print ("on the " + 
