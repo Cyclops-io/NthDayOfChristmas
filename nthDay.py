49cde16e9f90f1bf2a7bbd532c125aa58ba98886
Harry Warren <hpwarren@gmail.com>
2020-12-12 21:50:18 -0500

    mods

--- nthDay.py
+++ nthDay.py
@@ -1,18 +1,36 @@
-from num2words import num2words
+#!/usr/bin/env python
 
-giftArr = ["partridge in a pear-tree", "turtle doves", "french hens","colly birds","gold rings","geese a laying", "swans a swimming","maids a milking","drummers drumming","pipers piping","ladies dancing","lords a leaping"]
+from num2words import num2words
+import sys
+
+write = False
+if '-print' in sys.argv:
+    write = True
+
+giftArr = ["partridge in a pear-tree", "turtle doves", "french hens","colly birds","gold rings", \
+           "geese a laying", "swans a swimming","maids a milking","drummers drumming", \
+           "pipers piping","ladies dancing","lords a leaping"]
+
+text = []
 
 def printNthDay(n):
     if(n == 0):
-        print("a " + giftArr[n] + ".  ")
+        return str("a " + giftArr[n] + ".  ")
     elif(n == 1):
-            print(num2words(n + 1) + " " + giftArr[n] + ", and  ")
+        return str(num2words(n + 1) + " " + giftArr[n] + ", and  ")
     else:
-        print(num2words(n + 1) + " "  + giftArr[n] + ",  ")
+        return str(num2words(n + 1) + " "  + giftArr[n] + ",  ")
 
 if __name__ == "__main__":
     for i in range(1, 13):
-        print ("On the " + num2words(i, ordinal=True) + " day of christmas\nMy true love gave to me:  ")
+        word = num2words(i, ordinal=True).capitalize()
+        text.append(str(f"On the {word} day of christmas\nMy true love gave to me:  "))
         for j in range(i-1,-1,-1):
-            printNthDay(j)
-        print("\n   ")
+            text.append(printNthDay(j))
+        text.append(("\n   "))
+
+    with open('nthDay.txt', 'w') as f:
+        for t in text:
+            f.write(t + '\n')
+            if write: print(t)
+        print(' wrote nthDay.txt')
